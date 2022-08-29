79b68e3d01abd7f4d0269818e59c7a0cfc880f6d
DillonTuell <dillontuell1994@gmail.com>
2022-08-29 16:19:07 -0400

    push

---
+++ Basic Calculator/main.py
@@ -0,0 +1,55 @@
+# define the functions needed, add, sub, mul, div
+# print options to the user
+# ask for values
+# call the functions
+# while loop to continue the program until the user wants to exit
+
+def add(a, b):
+    answer = a + b
+    print(str(a) + "+" + str(b) + "=" + str(answer))
+
+def sub(a, b):
+    answer = a - b
+    print(str(a) + "-" + str(b) + "=" + str(answer))
+
+def mul(a, b):
+    answer = a * b
+    print(str(a) + "*" + str(b) + "=" + str(answer))
+
+def div(a,b):
+    answer = a / b
+    print(str(a) + "/" + str(b) + "=" + str(answer))
+
+print("A. addition")
+print("B. subtraction")
+print("C. division")
+print("D. multiplication")
+print("E. Exit")
+
+choice = input("Input your choice: ")
+
+if choice == "a" or choice == "A":
+    print("Addition")
+    a = int(input("Input first number: "))
+    b = int(input("Input second number: "))
+    add(a,b)
+elif choice == "b" or choice =="B":
+    print("Subtraction")
+    a = int(input("Input first number: "))
+    b = int(input("Input second number: "))
+    sub(a, b)
+elif choice == "c" or choice =="C":
+    print("Multiplication")
+    a = int(input("Input first number: "))
+    b = int(input("Input second number: "))
+    mul(a, b)
+elif choice == "d" or choice =="D":
+    print("Division")
+    a = int(input("Input first number: "))
+    b = int(input("Input second number: "))
+    div(a, b)
+elif choice == "e" or choice == "E":
+    print("Program ended")
+    quit()
+
+
---
+++ Word Replacement Program/main.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "Hi guys, I am Dillon. I speak some French and enjoy exercising"
+    word_to_replace = input("Enter word to be replaced: ")
+    word_replacement = input("Enter the replacement word: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
