61f8fdf66bde47bac97c7c7543bd9022748dd3c9
DillonTuell <dillontuell1994@gmail.com>
2022-08-30 12:25:35 -0400

    Update files

---
+++ Quiz Program/main.py
@@ -0,0 +1,71 @@
+# a dictionary that stores questions and answers
+# have a variable that tracks the score of the player
+# loop through the dictionary using the key value pairs
+# display each question to the user and allow them to answer
+# tell them if they are correct or incorrect
+# show the final result when the quiz is completed
+
+quiz = {
+    "question1": {
+        "question": "What is the capital of France?",
+        "answer": "Paris"
+    },
+    "question2":{
+        "question": "What is the capital of Germany?",
+        "answer": "Berlin"
+    },
+    "question3": {
+        "question": "What is the capital of Italy?",
+        "answer": "Rome"
+    },
+    "question4": {
+        "question": "What is the capital of Spain?",
+        "answer": "Madrid"
+    },
+    "question5":{
+        "question": "What is the capital of Portugal?",
+        "answer": "Lisbon"
+    },
+    "question6": {
+        "question": "What is the capital of Switzerland?",
+        "answer": "Bern"
+    },
+    "question7": {
+        "question": "What is the capital of Austria?",
+        "answer": "Vienna"
+    },
+}
+
+score = 0
+
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input("Answer: ")
+
+    if answer.lower() == value['answer'].lower():
+        print('Correct')
+        score = score + 1
+        print("Your score is: " + str(score))
+        print("")
+        print("")
+
+    else:
+        print("Wrong!")
+        print("The answer is: " + value['answer'])
+        print("Your score is: " + str(score))
+        print("")
+        print("")
+
+    if score == 7:
+        print('Congratulations! You got a perfect score!')
+
+    elif score <= 6:
+        print("Keep trying!")
+
+
+
+
+
+
+
+
